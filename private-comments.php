d6ede42629155891c4b0726baae6f0c71133579d
scotchfield <swrittenb@gmail.com>
2015-07-22 14:24:17 -0400

    Better comment filtering; use a single query instead of an unknown amount.

--- private-comments.php
+++ private-comments.php
@@ -42,10 +42,16 @@ class WP_PrivateComments {
 		add_filter( 'comments_array', array( $this, 'filter_comments' ) );
 	}
 
+	/**
+	 * Return a reference to the instance.
+	 */
 	public static function get_instance() {
 		return self::$instance;
 	}
 
+	/**
+	 * Display the checkbox on the user profile page.
+	 */
 	public function user_profile( $user ) {
 		$private = get_user_meta( $user->ID, $this->option, true );
 		$checked = ( $private == true ) ? 'checked' : '';
@@ -65,26 +71,50 @@ class WP_PrivateComments {
 <?php
 	}
 
+	/**
+	 * Call set_private_meta to update the metadata and toggle the privacy flag on the user's comments.
+	 */
 	public function user_profile_update( $user_id ) {
-		$private = false;
-
 		if ( isset( $_POST[ 'private_comments' ] ) ) {
-			update_user_meta( $user_id, $this->option, true );
-			$private = true;
+			$this->set_private_meta( $user_id, true );
 		} else {
-			update_user_meta( $user_id, $this->option, false );
+			$this->set_private_meta( $user_id, false );
 		}
 
-		$comments = get_comments( array( 'post_author' => $user_id ) );
-
-		foreach ( $comments as $comment ) {
-			update_comment_meta( $comment->comment_ID, $this->option, $private );
-		}
 	}
 
+	/**
+	 * Check the comment metadata to see if there are any marked as private.
+	 * If the current user is an admin, they can see all comments, but they'll be prefixed as private.
+	 * If the current user runs into their own private comment, prefix as private, but show it.
+	 * Anyone else will have the comment filtered out.
+	 */
 	public function filter_comments( $comments ) {
+		global $wpdb;
+
+		$comment_ids = array();
+
+		foreach ( $comments as $comment ) {
+			array_push( $comment_ids, intval( $comment->comment_ID ) );
+		}
+
+		// Set up a single database query instead of an unknown number of get_comment_meta calls.
+		$private_comment_ids = array();
+
+		$comment_obj = $wpdb->get_results(
+			$wpdb->prepare(
+				"SELECT comment_ID FROM {$wpdb->prefix}commentmeta WHERE meta_key='%s' AND meta_value='1' AND comment_ID IN (" . implode( ',', $comment_ids ) . ")",
+				array( $this->option )
+			),
+			ARRAY_A
+		);
+
+		foreach ( $comment_obj as $comment ) {
+			array_push( $private_comment_ids, $comment[ 'comment_ID' ] );
+		}
+
 		foreach ( $comments as $k => $comment ) {
-			if ( get_comment_meta( $comment->comment_ID, $this->option ) == true ) {
+			if ( in_array( $comment->comment_ID, $private_comment_ids ) ) {
 				if ( current_user_can( 'manage_options' ) || $comment->user_id == get_current_user_id() ) {
 					$comment->comment_content = '(Private) ' . $comment->comment_content;
 					continue;
@@ -95,6 +125,19 @@ class WP_PrivateComments {
 		}
 
 		return $comments;
+	}
+
+	/**
+	 * Set the privacy toggle metadata for the specified user, and update the privacy flag on their comments.
+	 */
+	public function set_private_meta( $user_id, $private ) {
+		update_user_meta( $user_id, $this->option, $private );
+
+		$comments = get_comments( array( 'post_author' => $user_id ) );
+
+		foreach ( $comments as $comment ) {
+			update_comment_meta( $comment->comment_ID, $this->option, $private );
+		}
 	}
 }
 
