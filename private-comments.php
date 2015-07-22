686fcfbf0002c4b09fa38c8c76711885975110a6
scotchfield <swrittenb@gmail.com>
2015-07-21 21:49:58 -0400

    Add user profile option for setting comments to private.

--- private-comments.php
+++ private-comments.php
@@ -32,13 +32,44 @@ class WP_PrivateComments {
 		}
 
 		self::$instance = $this;
+
+		$this->option = 'private_comments';
+
+		add_action( 'show_user_profile', array( $this, 'user_profile' ) );
+		add_action( 'edit_user_profile', array( $this, 'user_profile' ) );
+		add_action( 'personal_options_update', array( $this, 'user_profile_update' ) );
 	}
 
 	public static function get_instance() {
 		return self::$instance;
 	}
 
+	public function user_profile( $user ) {
+		$private = get_user_meta( $user->ID, $this->option, true );
+		$checked = ( $private == true ) ? 'checked' : '';
 
+?>
+<table class="form-table">
+	<tr>
+		<th>Private Comments</th>
+		<td>
+			<label for="private_comments">
+				<input type="checkbox" name="private_comments" id="private_comments" <?php echo( $checked ); ?> />
+				Make my comments private
+			</label>
+		</td>
+	</tr>
+</table>
+<?php
+	}
+
+	public function user_profile_update( $user_id ) {
+		if ( isset( $_POST[ 'private_comments' ] ) ) {
+			update_user_meta( $user_id, $this->option, true );
+		} else {
+			update_user_meta( $user_id, $this->option, false );
+		}
+	}
 }
 
 $wp_private_comments = new WP_PrivateComments();
